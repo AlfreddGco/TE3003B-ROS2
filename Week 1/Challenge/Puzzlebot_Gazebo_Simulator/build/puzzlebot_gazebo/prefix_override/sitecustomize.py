884db17a1960d1981cd2863ad1675c2c421be8db
Alfredo G <alfredd.gco@gmail.com>
2023-04-18 20:47:57 -0600

    puzzblebot.xacro and puzzlebot_simple_world.launch working atm

--- Week 1/Challenge/Puzzlebot_Gazebo_Simulator/build/puzzlebot_gazebo/prefix_override/sitecustomize.py
+++
@@ -1,3 +0,0 @@
-import sys
-sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/home/alfredo/Documents/TE3003B/Week 1/Challenge/Puzzlebot_Gazebo_Simulator/install/puzzlebot_gazebo'
--- Week 1/Challenge/Puzzlebot_Gazebo_Simulator/build/puzzlebot_world/share/puzzlebot_world/hook/pythonpath_develop.ps1
+++
@@ -1,3 +0,0 @@
-# generated from colcon_powershell/shell/template/hook_prepend_value.ps1.em
-
-colcon_prepend_unique_value PYTHONPATH "$env:COLCON_CURRENT_PREFIX\/home/alfredo/Documents/TE3003B/Week 1/Challenge/Puzzlebot_Gazebo_Simulator/build/puzzlebot_world"
