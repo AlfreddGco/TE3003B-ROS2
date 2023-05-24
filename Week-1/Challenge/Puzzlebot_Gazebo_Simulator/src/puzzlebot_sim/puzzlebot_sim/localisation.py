29b374a3046a6484e16b2d18eef7311d44241835
Andres Estrada <andrestradav@gmail.com>
2023-05-23 22:53:29 -0600

    localisation working now

--- Week-1/Challenge/Puzzlebot_Gazebo_Simulator/src/puzzlebot_sim/puzzlebot_sim/localisation.py
+++ Week-1/Challenge/Puzzlebot_Gazebo_Simulator/src/puzzlebot_sim/puzzlebot_sim/localisation.py
@@ -14,33 +14,22 @@ from tf_transformations import quaternion_from_euler
 class Localisation(Node):
     def __init__(self):
         super().__init__('puzzlebot_localisation')
-        self.get_logger().info('init...')
+        self.get_logger().info('localisation init...')
     
-        # Create a Rate object with a frequency of 60 Hz
-        self.rate = self.create_rate(60)
+        self.rate = 1.0 / 100.0
+        self.rate_creation = self.create_rate(100)
 
-        self.wl, self.wr = 0.0 , 0.0
+        self.wl, self.wr = .0 , .0
         self.kr, self.kl = 100.0, 100.0
-        self.wheel_radius = 0.05
-        self.wheel_distance = 0.08
-        self.pose = np.array([0, 0, 0], dtype=np.float64)
+        self.l, self.r = .08, .05
+        self.pose = np.empty((3,1))
+        self.sigmak = np.empty((3,3))
+        self.covariance_matrix = np.array([0.0]*36)
         self.create_subscription(Float32, '/wl', self.update_wl, 10)
         self.create_subscription(Float32, '/wr', self.update_wr, 10)
         self.odom = self.create_publisher(Odometry, '/odom', 10)
-        # Define covariance matrix
-        self.covariance_matrix = np.array([0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-                                  0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-                                  0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-                                  0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-                                  0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-                                  0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
-        self.Qk = np.array([
-            [0.5, 0.01, 0.01],
-            [0.01, 0.5, 0.01],
-            [0.01, 0.01, 0.2]
-        ] )
-        self.Hk = np.identity(3)
-        self.sigmak = np.zeros(3)
+        self.odomConstants = self.create_odomerty_msg()
+
     
     def update_wl(self, wl: Float32):
         self.wl = wl.data
@@ -48,18 +37,35 @@ class Localisation(Node):
     def update_wr(self, wr: Float32):
         self.wr = wr.data
 
-    def calculcate_covariance(self, deltaD, dt):
-        self.get_logger().info('calculating covariance...')
-        self.Hk[0][2] = -deltaD * math.sin(self.pose[2])/dt
-        self.Hk[1][2] = deltaD * math.cos(self.pose[2])/dt
-        wk = self.wheel_radius/(2*dt)*np.array([[math.cos(self.pose[2]),math.cos(self.pose[2])],
-                                           [math.sin(self.pose[2]),math.sin(self.pose[2])],
-                                           [2/self.wheel_distance, -2/self.wheel_distance]])
-        Edk = np.array([[self.kr*abs(self.wr), 0],
-                       [0, self.kl*abs(self.wl)]])
+    def get_hk(self) -> np.ndarray:
+        angle = self.pose[2].item()
+        val1 = -self.v * math.sin(angle) / self.rate
+        val2 = self.v * math.cos(angle) / self.rate
+        return np.array([[1, 0, val1],
+                         [0, 1, val2],
+                         [0, 0, 1]])
 
-        self.Qk = np.dot(np.dot(wk,Edk),np.transpose(wk))
-        self.sigmak = np.dot(np.dot(self.Hk, self.sigmak), np.transpose(self.Hk)) + self.Qk
+    def get_qk(self) -> np.ndarray:
+        angle = self.pose[2].item()
+        dwk = (self.r/(2*self.rate))*np.array([[math.cos(angle), math.cos(angle)],
+                       [math.sin(angle), math.sin(angle)],
+                       [2/self.l, -2/self.l]])
+        sdk = np.array([[self.kr*abs(self.wr), 0],
+                        [0, self.kl*abs(self.wl)]])
+        qk = np.dot(np.dot(dwk,sdk),dwk.T)
+        return qk
+
+    def get_point_msg(self, x_pose, y_pose, z_pose):
+        point_msg = Point()
+        point_msg.x = x_pose
+        point_msg.y = y_pose
+        point_msg.z = z_pose
+        return point_msg
+
+    def calculcate_covariance(self):
+        hk = self.get_hk()
+        Qk =  self.get_qk()
+        self.sigmak = np.dot(np.dot(hk, self.sigmak), hk.T) + Qk
         self.odomConstants.pose.covariance[0] = self.sigmak[0][0] 
         self.odomConstants.pose.covariance[1] = self.sigmak[0][1]
         self.odomConstants.pose.covariance[5] = self.sigmak[0][2]
@@ -71,14 +77,13 @@ class Localisation(Node):
         self.odomConstants.pose.covariance[35] = self.sigmak[2][2]
         
     def create_odomerty_msg(self)-> Odometry:
-        self.get_logger().info('creating odometry msgs...')
         odometry = Odometry()
-        odometry.header.stamp = self.time_now.to_msg()
+        odometry.header.stamp = self.get_clock().now().to_msg()
         odometry.header.frame_id = 'base_link'
         odometry.child_frame_id = 'chassis'
         odometry.pose.pose.position.x = 0.0
         odometry.pose.pose.position.y = 0.0
-        odometry.pose.pose.position.z = (self.wheel_radius)
+        odometry.pose.pose.position.z = (self.r)
         odometry.pose.pose.orientation.x = 0.0
         odometry.pose.pose.orientation.y = 0.0
         odometry.pose.pose.orientation.z = 0.0
@@ -94,33 +99,33 @@ class Localisation(Node):
         odometry.twist.covariance = self.covariance_matrix
         return odometry
     
-    def run(self):
-        self.get_logger().info("run ...")     
-        
+    def run(self):        
         while rclpy.ok():
             self.time_now = self.get_clock().now()
-            self.odomConstants = self.create_odomerty_msg()
-            dt = 1.0 / 60.0  # Assuming a fixed rate of 60 Hz
+            
+            self.v = self.r * (self.wr + self.wl) / 2 
+            self.w = self.r * (self.wr - self.wl) / self.l
 
-            l = self.wheel_distance
-            mat = np.array([[.5, .5], [1 / l, -1 / l]])
-            deltaD, deltaTheta = np.dot(mat, np.array([self.wr, self.wl])*self.wheel_radius)
-            self.calculcate_covariance(deltaD, dt)
-            self.pose += np.array([deltaD * math.cos(self.pose[2]), deltaD * math.sin(self.pose[2]), deltaTheta])/dt
+            angle = self.pose[2].item() 
+            self.pose += np.array([[self.v * math.cos(angle)], 
+                        [self.v * math.sin(angle)],
+                        [self.w]])/self.rate 
+            
+            self.calculcate_covariance()
 
-            self.odomConstants.pose.pose.position = Point(self.pose[0], self.pose[1], self.wheel_radius)
-            self.get_logger().info("odomconstants ok ...") 
-            q = quaternion_from_euler(0, 0, self.pose[2])
-            self.odomConstants.header.stamp = self.time_now.to_msg()
+            self.odomConstants.pose.pose.position = self.get_point_msg(self.pose[0].item() , self.pose[1].item() , self.r)
+            
+            q = quaternion_from_euler(0, 0, self.pose[2].item())
+            self.odomConstants.header.stamp = self.time_now.to_msg() #time stamp
             self.odomConstants.pose.pose.orientation.x = q[0]
             self.odomConstants.pose.pose.orientation.y = q[1]
             self.odomConstants.pose.pose.orientation.z = q[2]
             self.odomConstants.pose.pose.orientation.w = q[3]
-            self.odomConstants.twist.twist.linear.x = deltaD
-            self.odomConstants.twist.twist.angular.z = deltaTheta
+            self.odomConstants.twist.twist.linear.x = self.v
+            self.odomConstants.twist.twist.angular.z = self.w
             self.odom.publish(self.odomConstants)
 
-            self.rate.sleep()
+            self.rate_creation.sleep()
 
 def main():
     rclpy.init()
