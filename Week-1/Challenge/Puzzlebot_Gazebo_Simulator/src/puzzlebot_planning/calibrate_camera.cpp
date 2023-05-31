08583b35f101600698556db3cd2ab77f0d80fbd5
Alfredo Garcia <alfredd.gco@gmail.com>
2023-05-31 02:25:12 -0600

    Improvements to calibrate_camera

--- Week-1/Challenge/Puzzlebot_Gazebo_Simulator/src/puzzlebot_planning/calibrate_camera.cpp
+++ Week-1/Challenge/Puzzlebot_Gazebo_Simulator/src/puzzlebot_planning/calibrate_camera.cpp
@@ -1,16 +1,27 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/calib3d/calib3d.hpp>
+#include <opencv2/core/persistence.hpp>
 #include <iostream>
+
+void printMatrix(cv::Mat& mat, std::string prefix = "K"){
+	cv::MatIterator_<double> _it = mat.begin<double>();
+	std::cout << prefix << " = [";
+	for(; _it != mat.end<double>(); _it++){
+    		std::cout << *_it << " ";
+	}
+	std::cout << "]" << std::endl;
+}
 
 int main(){
 	cv::Mat image, image_gray;
+	const char *pipeline = " tcambin serial=15810833 ! video/x-raw, format=BGRx, width=1280,height=960, framerate=25/1 ! videoconvert ! appsink";
 	cv::VideoCapture input(0);
 
 	std::vector<cv::Point2f> corners_image;
 	std::vector< std::vector<cv::Point2f> > points_image;
 
-	cv::Size pattern_size(8, 6);
+	cv::Size pattern_size(7, 5);
 
 	for(;;){
 		if(!input.read(image)){
@@ -18,6 +29,7 @@ int main(){
 		}
 
 		cv::cvtColor(image, image_gray, cv::COLOR_BGR2GRAY);
+		cv::imshow("gray", image_gray);
 		bool found = cv::findChessboardCorners(image_gray, pattern_size, corners_image);
 		if(found){
 			cv::drawChessboardCorners(image, pattern_size, corners_image, found);
@@ -25,8 +37,9 @@ int main(){
 		}
 
 		cv::imshow("image", image);
-		char c = cv::waitKey(0) & 0xFF;
-		if(c==27){
+
+		char c = cv::waitKey(1) & 0xFF;
+		if(c == 27){
 			break;
 		}
 	}
@@ -44,11 +57,26 @@ int main(){
 	std::vector<cv::Mat> Rs, Ts;
 	double rms = cv::calibrateCamera(points_world, points_image, image_gray.size(), K, D, Rs, Ts);
 	std::cout << "Reprojection error: " << rms << std::endl;
+	
+	
+	printMatrix(K, "K");
+	printMatrix(D, "D");
+	
+	for(auto &r : Rs){
+		printMatrix(r, "Rs");
+	}
+
+	for(auto &t : Ts){
+		printMatrix(t, "Ts");
+	}
+
+	printf("Done Calibration\n");
 
 	cv::Mat undistorted;
 	cv::undistort(image_gray, undistorted, K, D);
 	cv::imshow("undistorted", undistorted);
-	cv::waitKey();
+	cv::imwrite("./undistored.png", undistorted);
+	cv::waitKey(0);
 
 	return 0;
 }
