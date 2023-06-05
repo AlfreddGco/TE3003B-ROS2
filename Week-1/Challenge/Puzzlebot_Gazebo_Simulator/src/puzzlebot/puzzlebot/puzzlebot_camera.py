2ba56ac25c2e1efc898b05149aa636cd50f508f5
Alfredo G <alfredd.gco@gmail.com>
2023-06-05 14:07:07 -0600

    Improvements to puzzlebot_camera

--- Week-1/Challenge/Puzzlebot_Gazebo_Simulator/src/puzzlebot/puzzlebot/puzzlebot_camera.py
+++ Week-1/Challenge/Puzzlebot_Gazebo_Simulator/src/puzzlebot/puzzlebot/puzzlebot_camera.py
@@ -1,29 +1,44 @@
+import os, time
 import cv2
-import time
 from threading import Thread
 
-def implement_node(obj, nh):
-    obj.nh = nh
-    obj.create_publisher = nh.create_publisher
-    obj.create_subscription = nh.create_subscription
-    obj.create_timer = nh.create_timer
+def get_pipeline():
+    username = os.getlogin()
+    if(username == 'jetson'):
+        return 'nvarguscamerasrc ! video/x-raw(memory:NVMM), width=(int)640, height=(int)480,format=(string)NV12, framerate=(fraction)60/1 ! nvvidconv ! video/x-raw, format=(string)BGRx ! videoconvert !  appsink'
+    else:
+        return 0
 
 
 class PuzzlebotCamera:
-    def __init__(self, nh):
-        implement_node(self, nh)
+    def __init__(self):
         self.frame = None
-        self.cap = cv2.VideoCapture(0)
+        self.cap = cv2.VideoCapture(get_pipeline())
         # create another thread for updating frame
         # with self.update_frame every 1/60 secs
-        self.updating_task = Thread(target=self.update_frames, args=())
+        self.running = True
+        self.updating_task = Thread(target=self._update_frames, args=())
         self.updating_task.start()
 
-    def update_frames(self):
-        while True:
+
+    def _update_frames(self):
+        while self.running:
             _, self.frame = self.cap.read()
-            time.sleep(1/60)
-    
-    def __del__(self):
-        self.updating_task.stop()
+            # TODO: use better timer. Take into account exec time
+            # of cap.read()
+            time.sleep(1/70)
+
+
+    def release(self):
+        self.running = False
+        self.updating_task.join()
         self.cap.release()
+    
+
+    def __del__(self):
+        self.release()
+
+
+if __name__ == '__main__':
+    camera = PuzzlebotCamera()
+
